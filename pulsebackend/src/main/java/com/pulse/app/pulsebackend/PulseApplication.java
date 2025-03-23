2361686db1127424f41894852e6c8d1cace92aa1
neilyml <55622147+neilyml@users.noreply.github.com>
2025-03-23 16:04:14 -0400

    create PulseHelloWorld

--- pulsebackend/src/main/java/com/pulse/app/pulsebackend/PulseApplication.java
+++ pulsebackend/src/main/java/com/pulse/app/pulsebackend/PulseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PulsebackendApplication {
+public class PulseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PulsebackendApplication.class, args);
+		SpringApplication.run(PulseApplication.class, args);
 	}
 
 }
---
+++ pulsebackend/src/main/java/com/pulse/app/pulsebackend/controllers/PulseHelloWorld.java
@@ -0,0 +1,12 @@
+package com.pulse.app.pulsebackend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PulseHelloWorld {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
