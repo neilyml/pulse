13f7baf61814aa08286d0749a3f2d35081e87e18
neilyml <55622147+neilyml@users.noreply.github.com>
2025-03-23 15:18:40 -0400

    init backend

---
+++ pulsebackend/src/main/java/com/pulse/app/pulsebackend/PulsebackendApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.app.pulsebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsebackendApplication.class, args);
+	}
+
+}
---
+++ pulsebackend/src/test/java/com/pulse/app/pulsebackend/PulsebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulse.app.pulsebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
