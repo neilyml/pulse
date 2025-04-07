52bee0cadc236c7e1c5d17b2f0fb512720dae7cb
neilyml <55622147+neilyml@users.noreply.github.com>
2025-04-06 20:56:48 -0400

    service, configmaps and everything

--- pulsebackend/src/main/java/com/pulse/app/pulsebackend/controllers/PulseHelloWorld.java
+++ pulsebackend/src/main/java/com/pulse/app/pulsebackend/controllers/PulseHelloWorld.java
@@ -1,12 +1,17 @@
 package com.pulse.app.pulsebackend.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PulseHelloWorld {
+
+
     @GetMapping("/hello")
     public String helloWorld() {
+        log.debug("Hello World Controller");
         return "Hello World";
     }
 }
